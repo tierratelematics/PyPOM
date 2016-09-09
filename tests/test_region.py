6c0e620b9a930a9fc2e3c52965b7cce16f06d44d
Davide Moro <davide.moro@gmail.com>
2016-09-09 17:51:02 +0200

    fix another test

--- tests/test_region.py
+++ tests/test_region.py
@@ -82,7 +82,9 @@ class TestRootElement:
         root_element.find_element.assert_called_once_with(*locator)
         selenium.find_element.assert_not_called()
 
-    def test_find_elements(self, page, selenium):
+    def test_find_elements_selenium(self, page, selenium, driver_interface):
+        skip_not_selenium(driver_interface)
+
         root_element = Mock()
         locator = (str(random.random()), str(random.random()))
         Region(page, root=root_element).find_elements(*locator)
@@ -144,6 +146,15 @@ class TestRootElement:
 
 
 class TestRootElementSplinter:
+
+    def test_find_elements_splinter(self, page, selenium, driver_interface, splinter_strategy):
+        skip_not_splinter(driver_interface)
+
+        root_element = MagicMock()
+        root_element.configure_mock(**{'find_by_{0}.return_value'.format(splinter_strategy): True})
+        locator = (splinter_strategy, str(random.random()))
+        Region(page, root=root_element).find_elements(*locator)
+        getattr(root_element, 'find_by_{0}'.format(splinter_strategy)).assert_called_once_with(locator[1])
 
     def test_is_element_present_splinter(self, page, selenium, driver_interface, splinter_strategy):
         skip_not_splinter(driver_interface)
