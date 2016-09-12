966eabc159de0314abb91266254c3bbd4bb15c6a
Davide Moro <davide.moro@gmail.com>
2016-09-12 13:14:08 +0200

    fix another test+

--- tests/test_page.py
+++ tests/test_page.py
@@ -130,13 +130,25 @@ def test_is_element_displayed(page, selenium):
     selenium.find_element.assert_called_once_with(*locator)
 
 
-def test_is_element_displayed_not_present(page, selenium):
+def test_is_element_displayed_not_present_selenium(page, selenium, driver_interface):
+    skip_not_selenium(driver_interface)
+
     locator = (str(random.random()), str(random.random()))
     from selenium.common.exceptions import NoSuchElementException
     selenium.find_element.side_effect = NoSuchElementException()
     assert not page.is_element_displayed(*locator)
     selenium.find_element.assert_called_once_with(*locator)
     selenium.find_element.is_displayed.assert_not_called()
+
+
+def test_is_element_displayed_not_present_splinter(page, selenium, driver_interface, splinter_strategy):
+    skip_not_splinter(driver_interface)
+
+    locator = (splinter_strategy, str(random.random()))
+    from splinter.element_list import ElementList
+    page.driver.configure_mock(**{'find_by_{0}.return_value'.format(splinter_strategy): ElementList([])})
+    assert not page.is_element_displayed(*locator)
+    getattr(page.driver, 'find_by_{0}'.format(splinter_strategy)).assert_called_once_with(locator[1])
 
 
 def test_is_element_displayed_not_displayed_selenium(page, selenium, driver_interface):
